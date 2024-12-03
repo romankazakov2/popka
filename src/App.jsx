7b28342928d913b6107219a33f19caf8ea8e45f5
Cybersport <Cybersport@26kadr.internal>
2024-12-03 16:41:00 +0300

    first commit22

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import logo from '../src/image/Logoooo.png';
+import tetka from './image/siduchiy tetka.png';
 import './App.css'; // Убедитесь, что вы импортируете файл стилей
 
 function App() {
     return (
-        <div className="flex flex-col items-center pt-1">
-            <p className="text-1xl  text-center">Summer sale up to 50% off</p>
-            <img src={logo} alt="Logo" className="mt-6" />
+      <>
+        <div className="flex flex-col items-center pt-1 bg-color bg-orange-500">
+            <p className="text-1xl  text-center ">Summer sale up to 50% off</p>
         </div>
+            <div>
+              <img src={logo} alt="" />
+            </div>
+            <div>
+            <img src={tetka} alt="" />
+            </div>
+            <div className="bg-color bg-orange-500">
+              <p>Numero</p>
+            </div>
+        
+      </>
     );
 }
 
